59c7adf9d300295f34da43694160e66d85eb0daf
Victor C Chinyeaka <vctrchinyeaka@gmail.com>
2024-07-09 22:54:56 +0100

    test: refactor to named parameters

--- lib/flutter_steganograph.dart
+++ lib/flutter_steganograph.dart
@@ -1,7 +1,2 @@
-library flutter_steganograph;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/flutter_steganograph.dart';
+export 'src/custom_exception.dart';
--- test/flutter_steganograph_test.dart
+++ test/flutter_steganograph_test.dart
@@ -12,7 +12,7 @@ void main() {
 
     setUp(() {
       steganograph = Steganograph();
-      
+
       coverImage = Image(width: 100, height: 100, numChannels: 4);
       secretImage = Image(width: 100, height: 100, numChannels: 4);
 
@@ -22,36 +22,44 @@ void main() {
 
     test('embedText and extractText', () {
       const text = "viktorvoltz!";
-      final imageWithText = steganograph.embedText(coverImage, text);
-      final extractedText = steganograph.extractText(imageWithText, text.length);
+      final imageWithText =
+          steganograph.embedText(image: coverImage, text: text);
+      final extractedText =
+          steganograph.extractText(image: imageWithText, length: text.length);
 
       expect(extractedText, equals(text));
     });
 
     test('embed and extract image', () {
-    for (int y = 0; y < coverImage.height; y++) {
-      for (int x = 0; x < coverImage.width; x++) {
-        secretImage.setPixel(x, y, ColorInt8.rgba(0, 255, 0, 255));
-        secretImage.setPixel(x, y, ColorInt8.rgba(0, 0, 255, 255));
-        secretImage.setPixel(x, y, ColorInt8.rgba(0, 0, 0, 255));
+      for (int y = 0; y < coverImage.height; y++) {
+        for (int x = 0; x < coverImage.width; x++) {
+          secretImage.setPixel(x, y, ColorInt8.rgba(0, 255, 0, 255));
+          secretImage.setPixel(x, y, ColorInt8.rgba(0, 0, 255, 255));
+          secretImage.setPixel(x, y, ColorInt8.rgba(0, 0, 0, 255));
+        }
       }
-    }
-    Image embeddedImage = steganograph.embedImage(coverImage, secretImage);
-    Image extractedImage = steganograph.extractImage(embeddedImage, secretImage.width, secretImage.height);
+      Image embeddedImage = steganograph.embedImage(
+        coverImage: coverImage,
+        secretImage: secretImage,
+      );
+      Image extractedImage = steganograph.extractImage(
+        embeddedImage: embeddedImage,
+        secretWidth: secretImage.width,
+        secretHeight: secretImage.height,
+      );
 
-    for (int y = 0; y < secretImage.height; y++) {
-      for (int x = 0; x < secretImage.width; x++) {
-        Color extractedPixel = extractedImage.getPixel(x, y);
-        Color originalPixel = secretImage.getPixel(x, y);
+      for (int y = 0; y < secretImage.height; y++) {
+        for (int x = 0; x < secretImage.width; x++) {
+          Color extractedPixel = extractedImage.getPixel(x, y);
+          Color originalPixel = secretImage.getPixel(x, y);
 
-        expect(extractedPixel.r, originalPixel.r);
-        expect(extractedPixel.g, originalPixel.g);
-        expect(extractedPixel.b, originalPixel.b);
-        expect(extractedPixel.a, originalPixel.a);
+          expect(extractedPixel.r, originalPixel.r);
+          expect(extractedPixel.g, originalPixel.g);
+          expect(extractedPixel.b, originalPixel.b);
+          expect(extractedPixel.a, originalPixel.a);
+        }
       }
-    }
-  });
-
+    });
   });
 }
 
@@ -65,5 +73,6 @@ void fillImage(Image image, Color color) {
 
 Color getRandomColor() {
   final random = Random();
-  return ColorInt8.rgba(random.nextInt(256), random.nextInt(256), random.nextInt(256), 255);
+  return ColorInt8.rgba(
+      random.nextInt(256), random.nextInt(256), random.nextInt(256), 255);
 }
